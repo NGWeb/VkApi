a989a08c88abee3a83c441a3e5fe2ed8c23d5ced
Сергей Щипанов <breate.by@gmail.com>
2014-04-13 21:31:00 +0300

    file reference fix

---
+++ Vk.SDK/Converters/AttachmentConverter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Vk.SDK.Model;
+
+namespace Vk.SDK.Converters
+{
+    public abstract class JsonCreationConverter<T> : JsonConverter
+    {
+        protected abstract T Create(Type objectType, JObject jsonObject);
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(T).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var jsonObject = JObject.Load(reader);
+            var target = Create(objectType, jsonObject);
+            serializer.Populate(jsonObject.CreateReader(), target);
+            return target;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //public class AttachmentListConverter : JsonCreationConverter<List<Attachment>>
+    //{
+    //    protected override List<Attachment> Create(Type objectType, JObject jsonObject)
+    //    {
+    //        return new List<Attachment>();
+    //    }
+    //}
+
+    public class AttachmentConverter : JsonCreationConverter<Attachment>
+    {
+
+        protected override Attachment Create(Type objectType, JObject jsonObject)
+        {
+            string typeName = (jsonObject["type"]).ToString();
+            switch (typeName)
+            {
+                case "photo":
+                    return new Model.Photo();
+                case "Audio":
+                    return new Audio();
+                case "link":
+                    return new Link();
+                case "album":
+                    return new PhotoAlbum();
+                default:
+                    return null;
+            }
+        }
+    }
+}
