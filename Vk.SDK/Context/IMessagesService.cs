3a46411c11d8597b01f0004b1e48a1218bb15fd6
Сергей Щипанов <breate.by@gmail.com>
2014-04-26 13:06:28 +0300

    messages

---
+++ Vk.SDK/Context/IMessagesService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vk.SDK.Model;
+
+namespace Vk.SDK.Context
+{
+    public interface IMessagesService
+    {
+        VKRequest<Array<Message>> Get(VKParameters parameters);
+        Array<Chat> GetDialogs(VKParameters parameters);
+        Array<Message> GetById(VKParameters parameters);
+        Array<Message> Search(VKParameters parameters);
+        Array<Message> GetHistory(VKParameters parameters);
+        VkJsonRequest Send(VKParameters parameters);
+        VkJsonRequest Delete(VKParameters parameters);
+        VkJsonRequest DeleteDialog(VKParameters parameters);
+        VkJsonRequest Restore(VKParameters parameters);
+        VkJsonRequest MarkAsRead(VKParameters parameters);
+        VkJsonRequest MarkAsImportant(VKParameters parameters);
+        VkJsonRequest GetLongPollServer(VKParameters parameters);
+        VkJsonRequest GetLongPollHistory(VKParameters parameters);
+        VkJsonRequest GetChat(VKParameters parameters);
+        VkJsonRequest CreateChat(VKParameters parameters);
+        VkJsonRequest EditChat(VKParameters parameters);
+        VkJsonRequest GetChatUsers(VKParameters parameters);
+        VkJsonRequest SetActivity(VKParameters parameters);
+        VkJsonRequest SearchDialogs(VKParameters parameters);
+        VkJsonRequest AddChatUser(VKParameters parameters);
+        VkJsonRequest RemoveChatUser(VKParameters parameters);
+        VkJsonRequest GetLastActivity(VKParameters parameters);
+        VkJsonRequest SetChatPhoto(VKParameters parameters);
+        VkJsonRequest DeleteChatPhoto(VKParameters parameters);
+    }
+}
---
+++ Vk.SDK/Context/MessagesService.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vk.SDK.Http;
+using Vk.SDK.Model;
+
+namespace Vk.SDK.Context
+{
+    public class MessagesService:VKApiBase,IMessagesService
+    {
+        public MessagesService(IRequestFactory factory) : base(factory)
+        {
+        }
+
+        public VKRequest<Array<Message>> Get(VKParameters parameters)
+        {
+            return PrepareRequest<Array<Message>>("get", parameters);
+        }
+
+        public Array<Chat> GetDialogs(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Array<Message> GetById(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Array<Message> Search(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Array<Message> GetHistory(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest Send(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest Delete(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest DeleteDialog(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest Restore(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest MarkAsRead(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest MarkAsImportant(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest GetLongPollServer(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest GetLongPollHistory(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest GetChat(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest CreateChat(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest EditChat(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest GetChatUsers(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest SetActivity(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest SearchDialogs(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest AddChatUser(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest RemoveChatUser(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest GetLastActivity(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest SetChatPhoto(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VkJsonRequest DeleteChatPhoto(VKParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
